497fbde703dd97d5fd175281fa625f1a0229aed1
Isaí Vázquez Valdez <92620535+IsaiVazqez@users.noreply.github.com>
2022-03-22 10:35:21 -0600

    autentificacion de datos

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:login/providers/login_form_provider.dart';
 import 'package:login/widgets/widgets.dart';
+import 'package:provider/provider.dart';
 
 import '../userinterface/input_decorations.dart';
 
@@ -18,7 +20,8 @@ class LoginScreen extends StatelessWidget {
                   SizedBox(height: 10),
                   Text('Login', style: Theme.of(context).textTheme.headline4),
                   SizedBox(height: 30),
-                  _LoginForm()
+                  ChangeNotifierProvider(
+                      create: (_) => LoginFormProvider(), child: _LoginForm())
                 ],
               )),
               SizedBox(
@@ -40,45 +43,77 @@ class LoginScreen extends StatelessWidget {
 class _LoginForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final loginForm = Provider.of<LoginFormProvider>(context);
     return Container(
       child: Form(
+          key: loginForm.formKey,
+          autovalidateMode: AutovalidateMode.onUserInteraction,
           child: Column(
-        children: [
-          TextFormField(
-            autocorrect: false,
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecorations.authInputDecoration(
-                hintText: 'isai@gmail.com',
-                labelText: 'Correo Electronico',
-                prefixIcon: Icons.alternate_email_rounded),
-          ),
-          SizedBox(height: 30),
-          TextFormField(
-            autocorrect: false,
-            obscureText: false,
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecorations.authInputDecoration(
-                hintText: '********',
-                labelText: 'Contraseña',
-                prefixIcon: Icons.lock_outline),
-          ),
-          SizedBox(height: 30),
-          MaterialButton(
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-            disabledColor: Colors.grey,
-            elevation: 0,
-            color: Colors.deepPurple,
-            child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15),
-                child: Text(
-                  'Ingresar',
-                  style: TextStyle(color: Colors.white),
-                )),
-            onPressed: () {},
-          ),
-        ],
-      )),
+            children: [
+              TextFormField(
+                autocorrect: false,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecorations.authInputDecoration(
+                    hintText: 'isai@gmail.com',
+                    labelText: 'Correo Electronico',
+                    prefixIcon: Icons.alternate_email_rounded),
+                onChanged: (value) => loginForm.email = value,
+                validator: (value) {
+                  String pattern =
+                      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+
+                  RegExp regExp = new RegExp(pattern);
+                  return regExp.hasMatch(value ?? '')
+                      ? null
+                      : 'El valor ingresado no es un correo';
+                },
+              ),
+              SizedBox(height: 30),
+              TextFormField(
+                autocorrect: false,
+                obscureText: false,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecorations.authInputDecoration(
+                    hintText: '********',
+                    labelText: 'Contraseña',
+                    prefixIcon: Icons.lock_outline),
+                onChanged: (value) => loginForm.password = value,
+                validator: (value) {
+                  return (value != null && value.length >= 6)
+                      ? null
+                      : 'La contraseña debe er de 6 caracteres';
+                },
+              ),
+              SizedBox(height: 30),
+              MaterialButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                disabledColor: Colors.grey,
+                elevation: 0,
+                color: Colors.deepPurple,
+                child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 80, vertical: 15),
+                    child: Text(
+                      loginForm.isLoading ? 'Espere' : 'Ingresar',
+                      style: TextStyle(color: Colors.white),
+                    )),
+                onPressed: loginForm.isLoading
+                    ? null
+                    : () async {
+                        FocusScope.of(context).unfocus();
+
+                        if (loginForm.isValidForm()) return;
+
+                        loginForm.isLoading = true;
+                        await Future.delayed(Duration(seconds: 2));
+
+                        loginForm.isLoading = false;
+
+                        Navigator.pushReplacementNamed(context, 'home');
+                      },
+              ),
+            ],
+          )),
     );
   }
 }
