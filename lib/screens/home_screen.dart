16885a5b3bb59ed94df3ffacb1ca6f527121bf9f
Isaí Vázquez Valdez <92620535+IsaiVazqez@users.noreply.github.com>
2022-03-23 12:20:58 -0600

    productCard

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:login/widgets/widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Home World'),
+      appBar: AppBar(title: Text('Productos')),
+      body: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (BuildContext context, int index) => ProductCard()),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
       ),
     );
   }
---
+++ lib/widgets/product_card.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class ProductCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Container(
+        margin: EdgeInsets.only(top: 30, bottom: 50),
+        width: double.infinity,
+        height: 400,
+        decoration: _cardBorders(),
+        child: Stack(
+          children: [_BackgroundImage()],
+        ),
+      ),
+    );
+  }
+
+  BoxDecoration _cardBorders() => BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(25),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black12, offset: Offset(0, 7), blurRadius: 10)
+          ]);
+}
+
+class _BackgroundImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(25),
+      child: Container(
+        width: double.infinity,
+        height: 400,
+        child: FadeInImage(
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          image: NetworkImage('https/via.placeholder.com/400x300/f6f6f6'),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
